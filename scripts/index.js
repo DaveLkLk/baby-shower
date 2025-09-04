16a7de6fd794eaab05c6659d6d1b5b2e37440b47
DaveLkLk <blueplanet98dev@gmail.com>
2025-09-04 00:36:36 -0500

    music added

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,6 @@ const fechaObjetivo = new Date("2025-09-20T19:00:00").getTime();
     const diferencia = fechaObjetivo - ahora;
 
     if (diferencia <= 0) {
-      // Si ya pasó la fecha
       document.getElementById("dias").innerText = "0";
       document.getElementById("horas").innerText = "0";
       document.getElementById("minutos").innerText = "0";
@@ -16,7 +15,6 @@ const fechaObjetivo = new Date("2025-09-20T19:00:00").getTime();
       return;
     }
 
-    // Cálculos de tiempo
     const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
     const horas = Math.floor(
       (diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -26,15 +24,20 @@ const fechaObjetivo = new Date("2025-09-20T19:00:00").getTime();
     );
     const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
 
-    // Actualizar en el DOM
     document.getElementById("dias").innerText = dias;
     document.getElementById("horas").innerText = horas;
     document.getElementById("minutos").innerText = minutos;
     document.getElementById("segundos").innerText = segundos;
   }
-
-  // Actualiza cada segundo
   const intervalo = setInterval(actualizarCuentaRegresiva, 1000);
-
-  // Llamada inicial
   actualizarCuentaRegresiva();
+
+  const btn = document.getElementById("music");
+  const audio = document.getElementById("bg-music");
+
+    audio.muted = true;
+
+    btn.addEventListener("click", () => {
+        audio.muted = !audio.muted;
+        btn.classList.toggle("active");
+    });
